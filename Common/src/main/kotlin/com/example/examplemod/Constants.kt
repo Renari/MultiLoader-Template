617380814b7538d2805f90281c74a21440995342
Arimil <renari@arimil.com>
2022-07-14 03:31:04 -0400

    fix modid and modname

--- Common/src/main/kotlin/com/example/examplemod/Constants.kt
+++ Common/src/main/kotlin/com/example/examplemod/Constants.kt
@@ -4,8 +4,8 @@ import org.apache.logging.log4j.LogManager
 
 class Constants {
     companion object {
-        const val MOD_ID = "examplemod"
-        const val MOD_NAME = "Example Mod"
+        const val MOD_ID = "multiloader"
+        const val MOD_NAME = "Multi Loader Template"
         val LOG = LogManager.getLogger(MOD_NAME)
     }
 }
